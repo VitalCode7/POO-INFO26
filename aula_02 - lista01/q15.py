6a2d2551e3712d13c35f0f41afc70dd00d65af58
VitalCode7 <abraaovital272@gmail.com>
2026-03-24 18:52:03 +0000

    1

--- aula_02 - lista01/q15.py
+++ aula_02 - lista01/q15.py
@@ -1,12 +1,16 @@
+import math
 def primo(n):
     primo = True
-    for i in range(2, n):
+    for i in range(2, math.ceil(n**(1/2))):
         if n % i == 0:
             primo = False
-    if primo == True:
+        if not primo: 
+            break
+    if primo:
         print(f"{n} é primo")
     else:
         print(f"{n} Não é primo")
+primo(11111111117)
 primo(3)
 primo(2)
 primo(4)
@@ -14,4 +18,4 @@ primo(7)
 primo(9)
 primo(11)
 primo(13)
-primo(15)
+primo(13)
---
+++ aula_04 - lista01/q1.py
@@ -0,0 +1,6 @@
+class circulo:
+    def __init__(self):
+        self.r = 0
+    def calc_area(self):
+        
+    def calc_circun(self):
